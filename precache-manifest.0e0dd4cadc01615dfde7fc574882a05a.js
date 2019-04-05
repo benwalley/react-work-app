3ceeb84bbe9b2f8dbeb0ac25ec468b5f6e3dd9bd
Ben Walley <benwalleyorigami@gmail.com>
2019-04-05 08:32:38 -0500

    Updates

---
+++ precache-manifest.0e0dd4cadc01615dfde7fc574882a05a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8389f3f7e9aab30efed",
+    "url": "/react-work-app/static/js/runtime~main.709aa85b.js"
+  },
+  {
+    "revision": "dc812b7be239b89fd899",
+    "url": "/react-work-app/static/js/main.d847cc6b.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-work-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "dc812b7be239b89fd899",
+    "url": "/react-work-app/static/css/main.97177082.chunk.css"
+  },
+  {
+    "revision": "9438a541a647e09ee1e4b8cddf5440a6",
+    "url": "/react-work-app/index.html"
+  }
+];
