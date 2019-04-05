29c3018c321ca56d05aa59e579bd444bcaf406bf
Ben Walley <benwalleyorigami@gmail.com>
2019-04-04 21:36:58 -0500

    fix description

--- src/Task.jsx
+++ src/Task.jsx
@@ -29,7 +29,7 @@ class Task extends React.Component {
 
   render() {
     return (
-      <div className={this.props.data.taskClass}>
+      <div draggable="true" className={this.props.data.taskClass}>
       	<div className="edit" onClick={this.handleOpenEditWindow}><i className="far fa-edit"></i></div>
         <h2 className="title">
         	{this.props.data.title}
@@ -45,7 +45,7 @@ class Task extends React.Component {
         </div>
         <div className="description section">
         	<div className="section-title">Description</div>
-        	<div className="section-data">{this.props.data.desctiption}</div>
+        	<div className="section-data">{this.props.data.description}</div>
         </div>
         <div className="delete" onClick={this.handleDelete}><i className="fas fa-trash-alt"></i></div>
         <div className="status section">
